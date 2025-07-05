8611f6946da950c8e0c9dbfa8c4f2ee7d04f88fc
dhlee <qwepoir12345@gmail.com>
2025-07-05 23:22:36 +0900

    [fix] 프리티어 적용안된 파일 적용 및 폰트 노토산스로 변경

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,17 @@
 // next.config.ts
-import type { NextConfig } from "next";
-import dotenvFlow from "dotenv-flow";
+import type { NextConfig } from 'next';
+
+import dotenvFlow from 'dotenv-flow';
 
 dotenvFlow.config();
 
 const nextConfig: NextConfig = {
-  reactStrictMode: process.env.NODE_ENV === "production",
+  reactStrictMode: process.env.NODE_ENV === 'production',
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
   images: {
-    domains: ["localhost", "stage.com", "production.com"],
+    domains: ['raw.githubusercontent.com', 'localhost', 'stage.com', 'production.com'],
   },
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Noto_Sans_KR } from 'next/font/google';
+
 import './globals.css';
 import { QueryProvider } from './providers/QueryProvider';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const notoSansKr = Noto_Sans_KR({
+  weight: ['400', '500', '700'],
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: '--font-noto-sans-kr',
 });
 
 export const metadata: Metadata = {
@@ -25,9 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${notoSansKr.variable} antialiased`}>
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
