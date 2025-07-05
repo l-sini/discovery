3084cd9cb68fa57505fb8c9fc145cef0c73cbd67
dhlee <qwepoir12345@gmail.com>
2025-07-05 22:54:19 +0900

    [feat] 비동기상태관리 tanstackquery 추가

---
+++ src/app/providers/QueryProvider.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 10000,
+      gcTime: 300000,
+      retry: 2,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: (err: Error) => {
+      console.error("API 요청 중 에러>>>", err);
+      throw err;
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: (err: Error) => {
+      console.error("API 요청 중 에러>>>", err);
+      throw err;
+    },
+  }),
+});
+
+interface QueryProviderProps {
+  children: ReactNode;
+}
+
+export function QueryProvider({ children }: QueryProviderProps) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
